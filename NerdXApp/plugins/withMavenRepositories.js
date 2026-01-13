6caa14d1ef2e3433174713555f7588935fed1781
Zimbwa03 <neezykidngoni@gmail.com>
2026-01-13 09:38:00 +0200

    "Update maven repository configuration"

--- NerdXApp/plugins/withMavenRepositories.js
+++ NerdXApp/plugins/withMavenRepositories.js
@@ -13,21 +13,21 @@ module.exports = function withMavenRepositories(config) {
 
 function addMavenRepositories(buildGradle) {
     const jfrogRepo = `maven { url "https://releases.jfrog.io/artifactory/oss-releases" }`;
+    const gradlePluginsRepo = `maven { url "https://plugins.gradle.org/m2/" }`;
 
     let newBuildGradle = buildGradle;
 
     // Add to buildscript repositories
-    // Using a regex to find the repositories block inside buildscript
     if (!newBuildGradle.includes('releases.jfrog.io')) {
         newBuildGradle = newBuildGradle.replace(
             /(buildscript\s*\{[\s\S]*?repositories\s*\{)/,
-            `$1\n        mavenCentral()\n        jcenter()\n        ${jfrogRepo}`
+            `$1\n        ${jfrogRepo}\n        mavenCentral()\n        ${gradlePluginsRepo}`
         );
 
         // Add to allprojects repositories
         newBuildGradle = newBuildGradle.replace(
             /(allprojects\s*\{[\s\S]*?repositories\s*\{)/,
-            `$1\n        mavenCentral()\n        jcenter()\n        ${jfrogRepo}`
+            `$1\n        ${jfrogRepo}\n        mavenCentral()\n        ${gradlePluginsRepo}`
         );
     }
 
